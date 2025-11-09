11b9c1e3efc228bfa54d61392edf099e142a34cc
Piotr Moch <piotr@MacBook-Air-Piotr.local>
2025-11-09 13:26:12 +0100

    Users

--- setup_users.py
+++ setup_users.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cr,")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm")
 django.setup()
 
 from django.contrib.auth import get_user_model
